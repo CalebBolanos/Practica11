d8d4951187026f1c8631d710415c9ccfdf400bf8
Caleb Bolaños <bolanos.c@hotmail.com>
2018-04-22 22:16:05 -0500

    Initial commit

---
+++ app/src/main/java/ipn/cecyt9/caleb/practica11/MainActivity.java
@@ -0,0 +1,22 @@
+package ipn.cecyt9.caleb.practica11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView texto = (TextView) findViewById(R.id.textView);
+        Animation animacion = AnimationUtils.loadAnimation(this, R.anim.animacion);
+        texto.startAnimation(animacion);
+
+    }
+}
